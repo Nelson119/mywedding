aaa50bb1d2ca7cbcfd8cc73805452a1be04648ae
nelson <nelson119@outlook.com>
2016-04-15 12:36:55 +0800

    lint

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,19 @@
-// console.log('\'Allo \'Allo!');
+'use strict';
+/*eslint-disable new-cap, no-unused-vars, 
+	no-use-before-define, no-trailing-spaces, space-infix-ops, comma-spacing,
+	no-mixed-spaces-and-tabs, no-multi-spaces, camelcase, no-loop-func,no-empty,
+	key-spacing ,curly, no-shadow, no-return-assign, no-redeclare, no-unused-vars,
+	eqeqeq, no-extend-native*/
+/*global  $ */
 $(function(){
 	$.parallaxify();
 	$('#coverflow').coverflow({index: 3, change: function(){
 	}});
-	var ul = $('<ul></ul>')
+	var ul = $('<ul></ul>');
 	$('#coverflow >*').each(function(){
 		var li = $('<li><a href=\'javascript:\'></a></li>');
 		li.appendTo(ul);
-	})
+	});
 	ul.insertAfter($('#coverflow'));
 
 });
