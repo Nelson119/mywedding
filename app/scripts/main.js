799c96fdacc69ae5ccfaf994da64c299ed492598
nelson <nelson119@outlook.com>
2016-04-15 12:28:49 +0800

    mywedding

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,13 @@
-console.log('\'Allo \'Allo!');
+// console.log('\'Allo \'Allo!');
 $(function(){
 	$.parallaxify();
+	$('#coverflow').coverflow({index: 3, change: function(){
+	}});
+	var ul = $('<ul></ul>')
+	$('#coverflow >*').each(function(){
+		var li = $('<li><a href=\'javascript:\'></a></li>');
+		li.appendTo(ul);
+	})
+	ul.insertAfter($('#coverflow'));
+
 });
