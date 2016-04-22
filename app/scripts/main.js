78f2dcb7a3c996bf14440da6ab5cf0220b80cdd8
nelson <nelson119@outlook.com>
2016-04-22 13:18:23 +0800

    0422

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,16 +4,61 @@
 	no-mixed-spaces-and-tabs, no-multi-spaces, camelcase, no-loop-func,no-empty,
 	key-spacing ,curly, no-shadow, no-return-assign, no-redeclare, no-unused-vars,
 	eqeqeq, no-extend-native*/
-/*global  $ */
+/*global  $, TweenMax, Waypoint */
 $(function(){
-	$.parallaxify();
-	$('#coverflow').coverflow({index: 3, change: function(){
-	}});
-	var ul = $('<ul></ul>');
-	$('#coverflow >*').each(function(){
-		var li = $('<li><a href=\'javascript:\'></a></li>');
-		li.appendTo(ul);
-	});
-	ul.insertAfter($('#coverflow'));
+	if($('body.home').length){
+		var ul = $('<ul></ul>');
+		$('#coverflow >*').each(function(){
+			var li = $('<li><a href=\'javascript:\'></a></li>');
+			li.on('click', function(){
+				// console.log($(this).index())
+				$('#coverflow').coverflow('index', $(this).index());
+			});
+			li.appendTo(ul);
+		});
+		ul.insertAfter($('#coverflow'));
+		$('#coverflow').coverflow({
+			index: 3, 
+			// innerAngle: 75,
+
+			change: function(container, element, index){
+				$('li', ul).eq(index).addClass('active').siblings().removeAttr('class');
+			}
+		});
+
+		$('.waypoint').each(function(i,d){
+			var a = $(document.createElement('a'));
+			var waypoint = new Waypoint({
+				element: d,
+				handler: function(direction) {
+					console.log(direction);
+					a.addClass('active');
+					a.siblings().removeClass('active');
+				}
+			});
+			a.on('click', function(){
+				var direction = $(window).scrollTop() < $(d).offset().top;
+				TweenMax.to('html, body', 0.5, {
+					scrollTop: $(d).offset().top + direction * 1
+				});
+			});
+			$('#waypoints').append(a);
+		});
+		$(window).on('resize scroll', function(){
+			if($(window).width() > 768){
+				$.parallaxify();
+			}else{
+				$.parallaxify('destroy');
+			}
+		});
+
+		$('.menu').on('click', function(){
+			if($('body').hasClass('menuon')){
+				$('body').removeClass('menuon');
+			}else{
+				$('body').addClass('menuon');
+			}
+		});
+	}
 
 });
